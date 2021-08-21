f1603adc966fc6fdeea0242b25c027c657187ce7
Алена <alena.lubanko@yandex.ru>
2021-08-21 10:05:58 +0300

    Layout create event page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
 import TableEditor from "./TableEditor/TableEditor";
+import {
+  BrowserRouter as Router, Switch, Route,
+} from 'react-router-dom';
+import CreateEvent from "./CreateEvent/CreateEvent";
+import RegFormBloger from "./RegFormBloger/RegFormBloger";
+import SpecCabinet from './SpecCabinet/SpecCabinet';
 
 function App() {
   return (
-    <div className="container">
-      <TableEditor />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div className="container">
+            <TableEditor />
+          </div>
+        </Route>
+        <Route exact path="/regBloger">
+          <RegFormBloger />
+        </Route>
+        <Route exact path="/SpecCabinet">
+          <SpecCabinet />
+        </Route>
+        <Route exact path="/eventNew">
+          <CreateEvent />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
